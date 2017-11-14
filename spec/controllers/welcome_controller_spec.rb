f698de3cb5f510c42b67d389e5fc8721fd21f54e
David Chavez <dvcv1992@gmail.com>
2017-11-14 03:19:53 -0800

    set up rpsec

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,12 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe WelcomeController, type: :controller do
+    let(:user) { FactoryGirl::create(:user) }
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+    before(:each) do
+        sign_in user
     end
-  end
 
+    describe "GET #index" do
+        it "returns http success" do
+            get :index
+            expect(response).to have_http_status(:success)
+        end
+    end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    
+     email 'regular@user.com'
+     password 'password'
+     password_confirmation 'password'
+     confirmed_at Time.zone.now
   end
 end
