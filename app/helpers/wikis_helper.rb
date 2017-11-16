8e75f3e5b309d73de94a7b9643cc52fdb49e0a72
David Chavez <dvcv1992@gmail.com>
2017-11-16 02:25:01 -0800

    Typo changed Delete to Private

--- app/helpers/wikis_helper.rb
+++ app/helpers/wikis_helper.rb
@@ -2,7 +2,7 @@ module WikisHelper
   def user_is_authorized_for_wiki?(wiki)
     current_user && (current_user == wiki.user || current_user.admin?)
   end
-  def user_is_authorized_for_delete_button?(wiki)
+  def user_is_authorized_for_private_button?(wiki)
     current_user.admin? || (current_user.premium? && current_user == wiki.user)
   end
 end
