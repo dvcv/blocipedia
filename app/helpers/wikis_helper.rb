6a310f1101cdc885fe2b57fe73f53b5cf8463087
David Chavez <dvcv1992@gmail.com>
2017-11-16 15:04:16 -0800

    Premium can now make new wikis private

--- app/helpers/wikis_helper.rb
+++ app/helpers/wikis_helper.rb
@@ -3,6 +3,6 @@ module WikisHelper
     current_user && (current_user == wiki.user || current_user.admin?)
   end
   def user_is_authorized_for_private_button?(wiki)
-    current_user.admin? || (current_user.premium? && current_user == wiki.user)
+    current_user.admin? || (current_user.premium? && current_user == wiki.user) || (current_user.premium? && current_page?(new_wiki_path(wiki)))
   end
 end
