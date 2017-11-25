6c324649e1444e04a67f7c152ac28a499452d7d6
David Chavez <dvcv1992@gmail.com>
2017-11-24 18:03:46 -0800

    Fixed issue where collaborators were shown when private button was not clicked

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,11 +1,15 @@
 $(document).ready(function(){
+    //Checks if private box is checked. If it is potential collaborators are shown.
+    var addcollaborators = function () {
+        if( $("#private").is(':checked') ){
+          $(".collaborators").show();
+        }else{
+          $(".collaborators").hide();
+        }
+    }
+    addcollaborators();
+
     $(".checkbox").click(function(){
-
-      if( $("#private").is(':checked') ){
-        $(".collaborators").show();
-      }else{
-        $(".collaborators").hide();
-      }
-
+      addcollaborators();
     });
 });
