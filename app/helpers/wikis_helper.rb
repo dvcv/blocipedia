b5efc76946cb2a2d22bc4dda33c73572ec26ebf4
David Chavez <dvcv1992@gmail.com>
2017-11-27 00:50:05 -0800

    More bug fixes

--- app/helpers/wikis_helper.rb
+++ app/helpers/wikis_helper.rb
@@ -5,4 +5,8 @@ module WikisHelper
   def user_is_authorized_for_private_button?(wiki)
     current_user.admin? || (current_user.premium? && current_user == wiki.user) || (current_user.premium? && current_page?(new_wiki_path(wiki)))
   end
+  def user_is_authorized_to_view_collaborators?(wiki)
+    current_uri = request.env['PATH_INFO']
+    ((wiki.user == current_user && current_user.premium?) || current_user.admin?) && (!(current_page?(new_wiki_path) ||  current_uri == "/wikis"))
+  end
 end
